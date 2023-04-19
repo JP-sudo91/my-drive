9db33a88a3de4332cad56e147d296b3ef1248736
Jayendra <parmarjayendra8860@gmail.com>
2023-04-19 23:28:38 +0530

    add the image

---
+++ src/components/PopUp/UploadFilesFolder.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const UploadFilesFolderContainer = styled('div')`
+
+`
+const UploadFilesFolder = () => {
+  return (
+    <UploadFilesFolderContainer>
+      <div className='upload-content'>
+        
+      </div>
+    </UploadFilesFolderContainer>
+  )
+}
+
+export default UploadFilesFolder
